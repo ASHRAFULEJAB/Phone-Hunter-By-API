7cbaa810259c3a2d3b61ca165c6d790045093c1a
Md.Ashraful Islam Khan <19103189@iubat.edu>
2022-09-01 23:02:06 +0600

    all-part-done

--- js/phone.js
+++ js/phone.js
@@ -1,19 +1,29 @@
-const phonesContainer = async(searhText) =>{
+const phonesContainer = async(searhText,dataLimit) =>{
   const url = `https://openapi.programming-hero.com/api/phones?search=${searhText}`
   const res = await fetch(url);
   const data = await res.json();
-  phoneLoading(data.data)
+  phoneLoading(data.data, dataLimit);
 }
 
-const phoneLoading = phones =>{
+const phoneLoading = (phones, dataLimit) =>{
     const phoneConatiner =  document.getElementById('phones-container');
     phoneConatiner.textContent='';
-    phones = phones.slice(0,15);
+
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 15){
+
+        phones = phones.slice(0,15);
+        showAll.classList.remove('d-none')
+    }
+    else{
+        showAll.classList.add('d-none')
+    }
 
     const notFound = document.getElementById('not-found');
     
     if(phones.length === 0){
        notFound.classList.remove('d-none')
+       
     }
     else{
         notFound.classList.add('d-none');
@@ -28,19 +38,78 @@ const phoneLoading = phones =>{
       <img src="${image}" class="card-img-top " alt="...">
       <div class="card-body">
         <h5 class="card-title">${phone_name}</h5>
-        <p class="card-text">${slug}</p>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, similique placeat ea quod minima quaerat cum voluptatibus alias, iste quia veritatis ipsa autem quidem necessitatibus at, odio modi odit eligendi!</p>
+        <button onclick="loadDetails('${slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailsModal">Show Details</button>
       </div>
     </div>
       
       `
     phoneConatiner.appendChild(phoneDiv);
     })
+    toggleSpinner(false);
+}
+
+const processSearch = dataLimit =>{
+
+    toggleSpinner(true)
+    const inputField = document.getElementById('input-field');
+    const searhText=inputField.value;
+    phonesContainer(searhText,dataLimit);
+    
+    
 }
 
 document.getElementById('btn-search').addEventListener('click',function(){
-    const inputField = document.getElementById('input-field');
-    const searhText=inputField.value;
-    inputField.value ='';
-    phonesContainer(searhText);
+
+    processSearch(15);
+    
 })
-// phonesContainer()
+
+document.getElementById('input-field').addEventListener('keypress',function(e){
+    if(e.key === 'Enter'){
+        processSearch(15);
+    }
+})
+
+const toggleSpinner = isLoading =>{
+    const loadingSpinner = document.getElementById('spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('d-none');
+    }
+    else{
+        loadingSpinner.classList.add('d-none');
+    }
+}
+
+document.getElementById('btn-show-all').addEventListener('click',function(){
+    processSearch();
+    // console.log(processSearch);
+//    processSearch.textContent='';
+})
+
+const loadDetails = async id =>{
+   const url =`https://openapi.programming-hero.com/api/phone/${id}`
+   const res = await fetch(url);
+   const data = await res.json();
+   displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone =>{
+    const {name, image,mainFeatures,others } = phone
+   const phoneModal =  document.getElementById('phoneDetailsModalLabel')
+   phoneModal.innerText = name;
+
+   const phoneModalDetails = document.getElementById('phone-modal-details');
+   phoneModalDetails.innerHTML=`
+        <img src="${image}"/>
+        <p> MainFeatures: ${mainFeatures.storage ? mainFeatures.storage : 'no Storage'}</p>
+        <p> MainFeatures: ${mainFeatures.memory ? mainFeatures.memory : 'no Memory'}</p>
+        <p> MainFeatures: ${mainFeatures.displaySize ? mainFeatures.displaySize : 'no displaySize'}</p>
+        <p>Sensors: ${mainFeatures.sensors ? mainFeatures.sensors[0] : 'no sensors'}</p>
+        <p> Others: ${others.Bluetooth ? others.Bluetooth : 'no Bluetooth'}</p>
+        <p> Others: ${others.WLAN ? others.WLAN : 'no WLAN'}</p>
+   `
+    
+}
+
+phonesContainer('apple')
